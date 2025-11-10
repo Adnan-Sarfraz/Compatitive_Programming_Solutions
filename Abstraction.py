7604b1cd1ab2d769c8c24664ce6ff154a035e5c9
Adnan-Sarfraz <145495367+Adnan-Sarfraz@users.noreply.github.com>
2025-11-10 18:29:42 +0500

    Add files via upload

---
+++ Abstraction.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+class shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass 
+
+class square (shape):
+    def __init__(self,side):
+        self.side=side 
+    
+    def area(self):
+        return self.side*self.side
+    
+sq=square(5)
+print(sq.area())
